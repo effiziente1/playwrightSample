cb73d0589bfb5faa29f757ea897b816beb94d2b3
abigailarmijohernandez <abi1521@hotmail.com>
2025-09-23 20:41:38 -0600

    fix bing maps test

--- tests/LocaleGeo/bingMaps.spec.ts
+++ tests/LocaleGeo/bingMaps.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from '@playwright/test';
 import { BingMapsPage } from '../../pages/Google/bingMapsPage';
-import * as allure from 'allure-js-commons';
 import { AnnotationType } from '../../utils/annotations/AnnotationType';
+import * as allure from 'allure-js-commons';
 
 test.describe('Geo Location Test', () => {
     //You need the longitude, latitude and geolocation permission to access to the geolocation
@@ -18,8 +18,8 @@ test.describe('Geo Location Test', () => {
         // eslint-disable-next-line playwright/no-skipped-test
         test.skip(browserName == 'firefox', 'Geolocation not work in firefox');
 
-        //This is the location label for the munich geolocation
-        const geoName = 'GermanyBavariaMunich (District)';
+        //This is the location label for the munich geolocation - using regex to match variations
+        const geoNamePattern = /GermanyBavariaMunich \(District\)(?:Munich)?/;
         const bingMapsPage = new BingMapsPage(page);
         await bingMapsPage.goTo();
         //With click in the locate button the map will be centered in your current geolocation
@@ -27,11 +27,11 @@ test.describe('Geo Location Test', () => {
         // eslint-disable-next-line playwright/no-conditional-in-test
         if (await bingMapsPage.unCollapse.isVisible())
             await bingMapsPage.unCollapse.click();
-        const assertionDescription = `Geo name is equal to: "${geoName}"`;
+        const assertionDescription = 'Geo name matches pattern';
         //Add the assertion to the html reporter annotations
         await bingMapsPage.addStepWithAnnotation(AnnotationType.Assert, assertionDescription, async () => {
             //Check the current geolocation label is set to Munich
-            await expect(bingMapsPage.geoName.locator, assertionDescription).toHaveText(geoName);
+            await expect(bingMapsPage.geoName.locator, assertionDescription).toHaveText(geoNamePattern);
         });
     });
 });
