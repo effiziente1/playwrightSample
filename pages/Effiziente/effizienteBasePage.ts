d5d63690907c1636e3a865379973904d4f5a7c2c
abigailarmijohernandez <abi1521@hotmail.com>
2026-01-23 21:31:37 -0600

    update packages

--- pages/Effiziente/effizienteBasePage.ts
+++ pages/Effiziente/effizienteBasePage.ts
@@ -5,32 +5,49 @@ import { AnnotationType } from '../../utils/annotations/AnnotationType';
 import { Menu } from '../../components/Menu';
 
 export class EffizienteBasePage extends BasePage {
-
     menu: Menu;
-    baseURL = process.env.EFFIZIENTE_URL ? process.env.EFFIZIENTE_URL : 'https://effizientedemo.azurewebsites.net';
-    message = new Generic(this.page, this.annotationHelper, '[data-test="message"]', 'Message');
+    baseURL = process.env.EFFIZIENTE_URL
+        ? process.env.EFFIZIENTE_URL
+        : 'https://abi-testing-dojo-demo.azurewebsites.net';
+    message = new Generic(
+        this.page,
+        this.annotationHelper,
+        '[data-test="message"]',
+        'Message',
+    );
 
-    constructor(page: Page, public readonly keyPage: string) {
-        //We need the page, and a friendly name for the page to be used in reports
+    constructor(
+        page: Page,
+    public readonly keyPage: string,
+    ) {
+    //We need the page, and a friendly name for the page to be used in reports
         super(page, keyPage);
         this.menu = new Menu(page, this.annotationHelper);
     }
 
     /**
-     * Check if the success message is visible
-     */
+   * Check if the success message is visible
+   */
     async checkSuccessMessage() {
         const assertDescription = 'Success message is visible';
-        await this.addStepWithAnnotation(AnnotationType.Assert, assertDescription, async () => {
-            await expect(this.message.locator, assertDescription).toBeVisible();
-        });
+        await this.addStepWithAnnotation(
+            AnnotationType.Assert,
+            assertDescription,
+            async () => {
+                await expect(this.message.locator, assertDescription).toBeVisible();
+            },
+        );
     }
 
     public async goTo() {
         const url = this.baseURL;
         const stepDescription = `Go to: "${url}"`;
-        await this.addStepWithAnnotation(AnnotationType.GoTo, stepDescription, async () => {
-            await this.page.goto(url);
-        });
+        await this.addStepWithAnnotation(
+            AnnotationType.GoTo,
+            stepDescription,
+            async () => {
+                await this.page.goto(url);
+            },
+        );
     }
 }
